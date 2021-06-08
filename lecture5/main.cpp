1ed2c0299825f6007a8b8bde24f82ad49f638237
DanylAnikushyn <danylanikushyn@gmail.com>
2021-06-08 22:34:14 +0300

    ctime changed to ctime_r; corrected mistakes with factory method

--- lecture5/main.cpp
+++ lecture5/main.cpp
@@ -10,88 +10,91 @@ class Logger
 {
 public:
     virtual ~Logger() {}; 
-    void log(const std::string& message) const
+    void log(const std::string& message)
     {
         auto current = std::chrono::system_clock::now();
         std::time_t current_time = std::chrono::system_clock::to_time_t(current);
-        char *time = ctime(&current_time);
-        if (time[strlen(time)-1] == '\n') time[strlen(time)-1] = '\0';
-        write(message, time);
+        char *buffer = new char[26];
+        ctime_r(&current_time, buffer);
+        if (buffer[strlen(buffer)-1] == '\n') buffer[strlen(buffer)-1] = '\0';
+        write(message, buffer);
+        delete[] buffer;
     }
 protected:
-    virtual void write(const std::string& message, const std::string& time) const = 0;
+    virtual void write(const std::string& message, const std::string& time) = 0;
 };
 
 class ConsoleLogger : public Logger 
 {
+public:
+    ConsoleLogger()
+    {
+        level = 0;
+    }
 protected:
-    void write(const std::string& message, const std::string& time) const override
+    void write(const std::string& message, const std::string& time) override
     {
         std::cout << level << ", " << time << ": [" << message << "]" << std::endl;
         ++level;
     }
 private:
-    static unsigned level;
+    unsigned level;
 };
-
-unsigned ConsoleLogger::level = 0;
 
 class FileLogger : public Logger
 {
-protected:
-    void write(const std::string& message, const std::string& time) const override
+public:
+    FileLogger()
     {
-        std::ofstream out;
+        level = 0;
         out.open("logs.txt", std::ofstream::out | std::ofstream::app);
+    }
+protected:
+    void write(const std::string& message, const std::string& time) override
+    {
         out << level << ", " << time << ": [" << message << "]" << std::endl;
         ++level;
-        out.close();
     }
 private:
-    static unsigned level;
+    unsigned level;
+    std::ofstream out;
 };
-
-unsigned FileLogger::level = 0;
 
 class OutputStream
 {
 public:
     virtual ~OutputStream() {};
-    virtual Logger* getLogger() const = 0;
-    void log(const std::string& message) const 
-    {
-        Logger* logger = this->getLogger();
-        logger->log(message);
-        delete logger;
-    }
+    virtual std::unique_ptr<Logger> getLogger() const = 0;
 };
 
 class ConsoleOutputStream : public OutputStream
 {
 public: 
-    Logger* getLogger() const override
+    std::unique_ptr<Logger> getLogger() const override
     {
-        return new ConsoleLogger();
+        return std::make_unique<ConsoleLogger>();
     }
 };
 
 class FileOutputStream : public OutputStream
 {
 public:
-    Logger* getLogger() const override
+    std::unique_ptr<Logger> getLogger() const override
     {
-        return new FileLogger();
+        return std::make_unique<FileLogger>();
     }
 };
 
 int main(int argc, char* argv[])
 {
     auto file_output = std::make_unique<FileOutputStream>();
-    file_output->log("hello");
+    auto file_logger = file_output->getLogger();
+    file_logger->log("hello");
     auto console_output = std::make_unique<ConsoleOutputStream>();
-    console_output->log("something happened 1");
-    file_output->log("world");
-    console_output->log("something happened 2");
-    console_output->log("something happened 3");
+    auto console_logger = console_output->getLogger();
+    console_logger->log("something happened 1");
+    file_logger->log("world");
+    console_logger->log("something happened 2");
+    console_logger->log("something happened 3");
     return 0;
 }
