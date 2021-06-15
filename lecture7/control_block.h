b54a1dc55cad70d65e8a588b983e55c75112c693
DanylAnikushyn <danylanikushyn@gmail.com>
2021-06-15 09:15:17 +0300

    my_shared_ptr with tests

---
+++ lecture7/control_block.h
@@ -0,0 +1,6 @@
+#pragma once
+
+struct ControlBlock
+{
+    unsigned ref_counter;
+};
--- lecture7/test.cpp
+++ lecture7/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "cobject.h"
 #include "my_unique_ptr.h"
+#include "my_shared_ptr.h"
 
 void test_my_unique_ptr()
 {
@@ -24,8 +25,29 @@ void test_my_unique_ptr()
     if (ptr2.get() && !ptr1.get()) std::cout << "ptr1 is moved into ptr2" << std::endl; 
 }
 
+void test_my_shared_ptr()
+{
+    auto ptr1 = MySharedPtr(new CObject(1));
+    {
+        auto ptr2(ptr1);
+        std::cout << "ptr1 ref count in scope: " << ptr1.get_count() << std::endl;
+        std::cout << "ptr2 ref count in scope: " << ptr2.get_count() << std::endl;
+    }
+    std::cout << "ptr1 ref count: " << ptr1.get_count() << std::endl;
+    auto ptr2(ptr1);
+    auto ptr3 = MySharedPtr(new CObject(3));
+    std::cout << "ptr2 before ptr2 = ptr3: ";
+    ptr2->print();
+    ptr2 = ptr3;
+    std::cout << "ptr1 ref count after ptr2 = ptr3: " << ptr1.get_count() << std::endl;
+    std::cout << "ptr3 ref count after ptr2 = ptr3: " << ptr2.get_count() << std::endl;
+    std::cout << "ptr2 after ptr2 = ptr3: ";
+    ptr2->print();
+}
+
 int main(int argc, char* argv[])
 {
-    test_my_unique_ptr();
+//    test_my_unique_ptr();
+    test_my_shared_ptr();
     return 0;
 }
